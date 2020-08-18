d663d77b049f9046929ab79d6cc5f63c782af583
vikramnatikar375 <69320231+vikramnatikar375@users.noreply.github.com>
2020-08-18 23:45:39 +0530

    Update Application.java

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -142,7 +142,7 @@ public class Application extends Controller {
         CompletionStage<List<Account>> getAccounts(AuthInfo authInfo) {
             CompletionStage<WSResponse> responsePromise = ws.url(authInfo.instanceUrl + "/services/data/v34.0/query/")
                     .addHeader("Authorization", "Bearer " + authInfo.accessToken)
-                    .addQueryParameter("q", "SELECT Id, Name, Type, Industry, Rating FROM Account")
+                    .addQueryParameter("q", "SELECT Id, Name, Type, Industry FROM Account")
                     .get();
 
             return responsePromise.thenCompose(response -> {
